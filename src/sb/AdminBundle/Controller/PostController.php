6261964160495a071930fb561904918481a046e8
fweber-de <florian.weber.dd@icloud.com>
2014-06-08 15:01:09 +0200

    admin routes

---
+++ src/sb/AdminBundle/Controller/PostController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace sb\AdminBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class PostController extends Controller
+{
+
+    public function collectionAction()
+    {
+
+    }
+
+    public function createAction()
+    {
+
+    }
+
+    public function updateAction()
+    {
+
+    }
+
+    public function deleteAction()
+    {
+
+    }
+
+}
